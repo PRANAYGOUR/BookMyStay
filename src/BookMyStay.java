8e4859498a1c0d27e5f35e23eb1c49842c9d9a99
PRANAYGOUR <pranaygour3042007@gmail.com>
2026-03-10 09:01:49 +0530

    UC1: added Version with Welcome message

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,4 +1,8 @@
 public class BookMyStay {
+    /*
+    @author Developer
+    @version 1.0
+    */
     public static void main(String[] args){
         System.out.println("Welcome to the hotel Booking Management System");
         System.out.println("System Initialized Successfully.");
